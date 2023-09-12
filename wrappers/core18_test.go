63320b8c2b53a369c9ff816bd4317297d34ccd95
Robert Ancell <robert.ancell@canonical.com>
2023-09-12 16:02:03 +0800

    wrappers: install snapd .desktop files on core
    https://github.com/snapcore/snapd/pull/13078

--- wrappers/core18_test.go
+++ wrappers/core18_test.go
@@ -69,6 +69,8 @@ func makeMockSnapdSnap(c *C) *snap.Info {
 		{"usr/share/dbus-1/services/io.snapcraft.Prompt.service", "[D-BUS Service]\nName=io.snapcraft.Prompt"},
 		{"usr/share/dbus-1/services/io.snapcraft.Settings.service", "[D-BUS Service]\nName=io.snapcraft.Settings"},
 		{"usr/share/dbus-1/services/io.snapcraft.SessionAgent.service", "[D-BUS Service]\nName=io.snapcraft.SessionAgent"},
+		// desktop files
+		{"usr/share/applications/io.snapcraft.SessionAgent.desktop", "[Desktop Entry]\nName=Snapd User Session Agent"},
 	})
 
 	return info
@@ -179,6 +181,9 @@ WantedBy=snapd.service
 	}, {
 		filepath.Join(dirs.SnapDBusSessionServicesDir, "io.snapcraft.SessionAgent.service"),
 		"[D-BUS Service]\nName=io.snapcraft.SessionAgent",
+	}, {
+		filepath.Join(dirs.SnapDesktopFilesDir, "io.snapcraft.SessionAgent.desktop"),
+		"[Desktop Entry]\nName=Snapd User Session Agent",
 	}} {
 		c.Check(entry[0], testutil.FileEquals, entry[1])
 	}
